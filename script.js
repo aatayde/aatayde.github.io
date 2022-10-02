c4e97d48b7f948b852999d7205d26365faf7687d
haribo <haribo@haribos-MBP.attlocal.net>
2022-10-02 03:50:56 -0700

    add function to remove & add classes

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("hello, world!")
+
+// store btn 
+const myButtons = document.getElementsByClassName("btn");
+
+function changeToDark(){
+    myButtons.classList.remove('btn-light')
+    myButtons.classList.add('btn-dark')
+}
+
+function changeToLight(){
+    myButtons.classList.remove('btn-dark')
+    myButtons.classList.add('btn-light')
+}
