a57ee66296a685fad8cdd20679636e7cf1ac9217
haribo <haribo@haribos-MacBook-Pro.local>
2022-10-06 13:19:29 -0700

    add for each loop in change theme functions

--- script.js
+++ script.js
@@ -6,22 +6,36 @@ const isDarkMode = window.matchMedia("(prefers-color-scheme: dark)");
 const myButtons = document.querySelectorAll(".btn");
 
 function changeToDark(){
-    myButtons.classList.remove('btn-light')
-    myButtons.classList.add('btn-dark')
+    // myButtons.classList.remove('btn-light')
+    // myButtons.classList.add('btn-dark')
+
+    console.log("Dark Mode triggered")
+    
+    myButtons.forEach(button => {
+        button.classList.remove('btn-light')
+        button.classList.add('btn-dark')
+      })
 }
 
 function changeToLight(){
-    myButtons.classList.remove('btn-dark')
-    myButtons.classList.add('btn-light')
+    // myButtons.classList.remove('btn-dark')
+    // myButtons.classList.add('btn-light')
+
+    console.log("light Mode triggered")
+    
+
+    myButtons.forEach(button => {
+        button.classList.remove('btn-light')
+        button.classList.add('btn-dark')
+      })
 }
 
 function changeThemes(e){
     return (e.matches ? changeToDark() : changeToLight())
+    
 }
 
 isDarkMode.addEventListener("change", e => {
-    myButtons.forEach(button => {
-        changeThemes(e)
-      })
+    changeThemes(e);
 })
 
