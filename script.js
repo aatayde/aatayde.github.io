091b611dd399472e786fac8200ee0397dff6c5bd
haribo <haribo@haribos-MacBook-Pro.local>
2022-10-05 21:00:52 -0700

    select btn class, remove & add classes for theme

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 console.log("hello, world!")
+const isDarkMode = window.matchMedia("(prefers-color-scheme: dark)");
+// const theButton = document.getElementById("btn-1")
 
 // store btn 
-const myButtons = document.getElementsByClassName("btn");
+const myButtons = document.querySelectorAll("a.btn");
 
 function changeToDark(){
     myButtons.classList.remove('btn-light')
@@ -12,3 +14,11 @@ function changeToLight(){
     myButtons.classList.remove('btn-dark')
     myButtons.classList.add('btn-light')
 }
+
+function changeThemes(e){
+    return (e.matches ? changeToDark() : changeToLight())
+}
+
+isDarkMode.addEventListener("change", e => {
+    changeThemes(e)
+})
